3c85382918a51df5f4dfaa3206dfcafec6a00cbc
Santiago Saavedra <santiagosaavedra@gmail.com>
2015-11-19 16:19:14 +0100

    Change error message into meaningful stuff

--- js/volunteer-signup.js
+++ js/volunteer-signup.js
@@ -43,13 +43,13 @@ $(function() {
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                error: function() {
+                error: function(xhr, textStatus, err) {
                   _paq.push(['trackEvent', 'commit-volunteer', 'failure'])
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Oops, " + firstName + ", parece que hubo un error. Si no consigues que funcione, mándanos un correo!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
