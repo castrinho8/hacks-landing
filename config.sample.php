a05404e35be57207587ca5d7fd68afe90ab1ebfe
Pablo Castro <castrinho8@gmail.com>
2017-02-20 01:03:11 +0100

    Add 2017 first commit

--- config.sample.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if(!defined('ENTRY_POINT')) return false;
-
-define('RECAPTCHA_APIKEY', 'not-needed-yet');
-define('RECAPTCHA_SECRET', 'your-recaptcha-secret-here');
-define('MAIL_FROM', 'user-sending-mail@host.example');
-define('MAIL_TO', 'user-receiving-mail@host.example');
